5da572dc6c5113002bf289ef3ba01ba4f08fc64c
rotari marcel <rotari.marcel@icloud.com>
2021-08-02 19:46:10 +0300

    Add project files.

---
+++ ItSchoolBank/cont.cpp
@@ -0,0 +1,10 @@
+#include "cont.h"
+
+ContBancar::ContBancar(std::string nume, std::string prenume, std::string IBAN)
+{
+	this->nume = nume;
+	this->prenume = prenume;
+	this->IBAN = IBAN;
+	sold = 0;
+	etipCont = TIP_CONT::CONT_RON;
+}
---
+++ ItSchoolBank/cont.h
@@ -0,0 +1,26 @@
+#pragma once
+#include<string>
+
+enum class TIP_CONT
+{
+	CONT_RON, 
+	CONT_EURO, 
+	CONT_DOLAR
+};
+
+class ContBancar
+{
+private:
+	std::string nume;
+	std::string prenume;
+	TIP_CONT etipCont;
+	std::string IBAN;
+	float sold;
+
+public:
+
+	ContBancar(std::string nume, std::string prenume, std::string IBAN);
+	~ContBancar();
+
+};
+
